fdac3127265097378cf87ef2bf888d6be65ea9d3
masha <masha.sukkur@gmail.com>
2019-04-27 16:25:44 +0000

    NYOT final project, college search

---
+++ script.js
@@ -0,0 +1,7 @@
+function x() {
+let request = document.getElementById('collegelist');
+let url = "https://api.data.gov/ed/collegescorecard/v1/schools?school.city=boston%20college&api_key=PWmBYMhuExKoK5KiAZEeLdV0YFaA8B4pbKp4MlT8";
+let userInput = document.getElementById('userInput').value;
+console.log(userInput);
+
+}
