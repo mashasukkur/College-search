9d01cadcf2b27e5784ae7af15637901c075aa970
mashasukkur <44121791+mashasukkur@users.noreply.github.com>
2019-05-04 12:46:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function x()
 let request = new XMLHttpRequest();
 let userinput = document.getElementById('userInput')
 
-let url = "https://api.data.gov/ed/collegescorecard/v1/schools?school.city=brooklyn%20college&api_key=aqrQOOIPPcld3m6PIIVjOzOOmGPu5aeDpo0gHi0n" + ${userinput};
+let url = `https://api.data.gov/ed/collegescorecard/v1/schools?school.city=${userinput.value}%20college&api_key=aqrQOOIPPcld3m6PIIVjOzOOmGPu5aeDpo0gHi0n`;
 
 
 
@@ -17,8 +17,8 @@ request.onload = function() {
     console.log(data);
     if(request.status >= 200 && request.status < 400)
     {
-        console.log(data);
-        document.getElementById("collegename").innerHTML= "College:" + results[2014].completion;
+        console.log(data.results["0"]["2014"]);
+        document.getElementById("collegename").innerHTML= "College:" + data.results[0].school.name;
     }
 }
 request.send();
